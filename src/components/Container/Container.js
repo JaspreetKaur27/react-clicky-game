0a988dcb89d21ea8958145f729a56783fd53c401
Jaspreet Kaur <jaspreetr86@gmail.com>
2018-09-27 19:28:37 -0400

    Updates

--- src/components/Container/Container.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// Main container element used to render the player cards
-const Container = props =>
-  <div className="container">
-        {props.children}
-  </div>;
-
-export default Container;
